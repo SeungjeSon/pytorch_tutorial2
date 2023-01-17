ee54ad4ee7fd57fdf69565746dbccf30ef40cd52
SJ <seungje0927@gmail.com>
2023-01-17 15:02:40 +0900

    add UNet architecture

---
+++ unet/train_unet.py
@@ -0,0 +1,133 @@
+## import lib
+import os
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from torchvision import transforms, datasets
+
+
+## 트레이닝 파라미터 설정하기
+lr = 1e-3
+batch_size = 4
+num_epoch = 100
+
+data_dir = './datasets'
+ckpt_dir = './checkpoint'
+log_dir = './log'
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+## 네트워크 구축
+class UNet(nn.Module):
+    def __init__(self):
+        super(UNet, self).__init__()
+
+        def CBR2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=True): # CBR : Convolution BatchNomalization Relu 약자
+            layer = []
+            layer += [nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)]
+            layer += [nn.BatchNorm2d(num_features=out_channels)]
+            layer += [nn.ReLU()]
+
+            cbr = nn.Sequential(*layer)
+
+            return cbr
+
+        # Contracting Path
+        self.enc1_1 = CBR2d(in_channels=1, out_channels=64)
+        self.enc1_2 = CBR2d(in_channels=64, out_channels=64)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc2_1 = CBR2d(in_channels=64, out_channels=128)
+        self.enc2_2 = CBR2d(in_channels=128, out_channels=128)
+
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc3_1 = CBR2d(in_channels=128, out_channels=256)
+        self.enc3_2 = CBR2d(in_channels=256, out_channels=256)
+
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc4_1 = CBR2d(in_channels=256, out_channels=512)
+        self.enc4_2 = CBR2d(in_channels=512, out_channels=512)
+
+        self.pool4 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc5_1 = CBR2d(in_channels=512, out_channels=1024)
+
+        #Expansive Path
+        self.dec5_1 = CBR2d(in_channels=1024, out_channels=512)
+
+        self.uppool4 = nn.ConvTranspose2d(in_channels=512, out_channels=512, kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec4_2 = CBR2d(in_channels=1024, out_channels=512)
+        self.dec4_1 = CBR2d(in_channels=512, out_channels=256)
+
+        self.uppool3 = nn.ConvTranspose2d(in_channels=256, out_channels=256, kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec3_2 = CBR2d(in_channels=512, out_channels=256)
+        self.dec3_1 = CBR2d(in_channels=256, out_channels=128)
+
+        self.uppool2 = nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec2_2 = CBR2d(in_channels=256, out_channels=128)
+        self.dec2_1 = CBR2d(in_channels=128, out_channels=64)
+
+        self.uppool1 = nn.ConvTranspose2d(in_channels=64, out_channels=64, kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec1_2 = CBR2d(in_channels=128, out_channels=64)
+        self.dec1_1 = CBR2d(in_channels=64, out_channels=64)
+
+        self.fc = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1, stride=1, padding=0, bias=True)
+
+    def forward(self, x):
+        enc1_1 = self.enc1_1(x)
+        enc1_2 = self.enc1_2(enc1_1)
+        pool1 = self.pool1(enc1_2)
+
+        enc2_1 = self.enc2_1(pool1)
+        enc2_2 = self.enc2_2(enc2_1)
+        pool2 = self.pool2(enc2_2)
+
+        enc3_1 = self.enc3_1(pool2)
+        enc3_2 = self.enc3_2(enc3_1)
+        pool3 = self.pool3(enc3_2)
+
+        enc4_1 = self.enc4_1(pool3)
+        enc4_2 = self.enc4_2(enc4_1)
+        pool4 = self.pool4(enc4_2)
+
+        enc5_1 = self.enc5_1(pool4)
+
+
+        dec5_1 = self.dec5_1(enc5_1)
+
+        uppool4 = self.uppool4(dec5_1)
+        # cat4 = torchvision.transforms.functional.center_crop(enc4_2, [uppool4.shape[2], uppool4.shape[3]])
+        cat4 = torch.cat((uppool4, enc4_2), dim=1)  # dim=[0: batch, 1: channel, 2: height, 3: weight]
+        dec4_2 = self.dec4_2(cat4)
+        dec4_1 = self.dec4_1(dec4_2)
+
+        uppool3 = self.uppool3(dec4_1)
+        cat3 = torch.cat((uppool3, enc3_2), dim=1)
+        dec3_2 = self.dec3_2(cat3)
+        dec3_1 = self.dec3_1(dec3_2)
+
+        uppool2 = self.uppool2(dec3_1)
+        cat2 = torch.cat((uppool2, enc2_2), dim=1)
+        dec2_2 = self.dec2_2(cat2)
+        dec2_1 = self.dec2_1(dec2_2)
+
+        uppool1 = self.uppool1(dec2_1)
+        cat1 = torch.cat((uppool1, enc1_2), dim=1)
+        dec1_2 = self.dec1_2(cat1)
+        dec1_1 = self.dec1_1(dec1_2)
+
+        x = self.fc(dec1_1)
+
+        return x
