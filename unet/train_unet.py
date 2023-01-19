f57b643b065ccb7a33f4abcd3d32fb0560746914
SJ <seungje0927@gmail.com>
2023-01-19 20:57:56 +0900

    add 네트워크 학습 part_2

--- unet/train_unet.py
+++ unet/train_unet.py
@@ -12,6 +12,8 @@ from torch.utils.tensorboard import SummaryWriter
 
 from torchvision import transforms, datasets
 
+import time
+
 
 ## 트레이닝 파라미터 설정하기
 lr = 1e-3
@@ -23,6 +25,7 @@ ckpt_dir = './checkpoint'
 log_dir = './log'
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
 
 ## 네트워크 구축
 class UNet(nn.Module):
@@ -85,7 +88,7 @@ class UNet(nn.Module):
         self.dec1_2 = CBR2d(in_channels=128, out_channels=64)
         self.dec1_1 = CBR2d(in_channels=64, out_channels=64)
 
-        self.fc = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1, stride=1, padding=0, bias=True)
+        self.fc = nn.Conv2d(in_channels=64, out_channels=1, kernel_size=1, stride=1, padding=0, bias=True)
 
     def forward(self, x):
         enc1_1 = self.enc1_1(x)
@@ -239,13 +242,13 @@ class RandomFlip(object):
 transform = transforms.Compose([Nomalization(mean=0.5, std=0.5), RandomFlip(), ToTensor()])
 
 dataset_train = Dataset(data_dir=os.path.join(data_dir, 'train'), transform=transform)
-loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=8)
+loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=0)
 
 dataset_val = Dataset(data_dir=os.path.join(data_dir, 'val'), transform=transform)
-loader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=True, num_workers=8)
+loader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=True, num_workers=0)
 
 ## 네트워크 생성
-net = UNet.to(device)
+net = UNet().to(device)
 
 ## 손실함수 정의하기
 fn_loss = nn.BCEWithLogitsLoss().to(device)
@@ -259,4 +262,123 @@ num_data_val = len(dataset_val)
 
 num_batch_train = np.ceil(num_data_train / batch_size)
 num_batch_val = np.ceil(num_data_val / batch_size)
+
+## 그밖에 부수적인 functions 설정하기
+fn_tonumpy = lambda x: x.to('cpu').detach().numpy().transpose(0, 2, 3, 1)   # from tensor to numpy
+fn_denorm = lambda x, mean, std: (x*std) + mean    # denormalization
+fn_class = lambda x: 1.0 * (x > 0.5)    # classification() using thresholding (p=0.5)
+
+## Tensorboard를 사용하기 위한 설정
+writer_train = SummaryWriter(log_dir=os.path.join(log_dir, 'train'))
+writer_val = SummaryWriter(log_dir=os.path.join(log_dir, 'val'))
+
+
+## 네트워크 저장하기
+def save(ckpt_dir, net, optim, epoch):
+    if not os.path.exists(ckpt_dir):
+        os.makedirs(ckpt_dir)
+
+    torch.save({'net': net.state_dict(), 'optim': optim.state_dict()}, "./%s/model_epoch%d.pth" % (ckpt_dir, epoch))
+
+# 네트워크 불러오기
+def load(ckpt_dir, net, optim):
+    if not os.path.exists(ckpt_dir):
+        epoch = 0
+        return net, optim, epoch
+
+    ckpt_lst = os.listdir(ckpt_dir)
+    ckpt_lst.sort(key=lambda f: int(''.join(str.isdigit, f)))
+
+    dict_model = torch.load('./%s/%s' % (ckpt_dir, ckpt_lst[-1]))
+
+    net.load_state_dict(dict_model['net'])
+    optim.load_state_dict(dict_model['optim'])
+    epoch = int(ckpt_lst[-1].split('epoch')[1].split('.pth'[0]))
+
+    return net, optim, epoch
+
+
+## 네트워크 학습시키기
+st_epoch = 0
+net, optim, st_epoch = load(ckpt_dir=ckpt_dir, net=net, optim=optim)
+
+for epoch in range(1, num_epoch+1):
+    net.train()
+
+    loss_arr = []
+    acc_arr = []
+
+    for batch, data in enumerate(loader_train, 1):
+        #forward pass
+        input = data['input'].to(device)
+        label = data['label'].to(device)
+
+        output = net(input)
+
+        #backward pass
+        optim.zero_grad()
+
+        loss = fn_loss(output, label)
+        loss.backward()
+
+        optim.step()
+
+        # 손실함수 계산
+        loss_arr += [loss.item()]
+
+        print("TRAIN: EPOCH %04d / %04d | BATCH %04d / %04d | LOSS %.4f" %
+              (epoch, num_epoch, batch, num_batch_train, np.mean(loss_arr)))
+
+        # Tensorboard 저장하기  (input, output, label 저장)
+        label = fn_tonumpy(label)
+        input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
+        output = fn_tonumpy(fn_class(output))
+
+        writer_train.add_image('label', label, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+        writer_train.add_image('input', input, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+        writer_train.add_image('output', output, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+
+    # loss를 Tensorboard에 저장
+    writer_train.add_scalar('loss', np.mean(loss_arr), epoch)
+
+
+    # validation 시작 부분
+    # val은 backpropagation이 없기 때문에 no_grad()사용
+    with torch.no_grad():
+        net.eval()
+        loss_arr = []
+
+        for batch, data in enumerate(loader_val, 1):
+            # forward pass
+            input = data['input'].to(device)
+            label = data['label'].to(device)
+
+            output = net(input)
+
+            # 손실함수 계산
+            loss = fn_loss(output, label)
+
+            loss_arr += [loss.item()]
+
+            print("VAL: EPOCH %04d / %04d | BATCH %04d / %04d | LOSS %.4f" %
+                  (epoch, num_epoch, batch, num_batch_train, np.mean(loss_arr)))
+
+            # Tensorboard 저장하기  (input, output, label 저장)
+            label = fn_tonumpy(label)
+            input = fn_tonumpy(fn_denorm(input, mean=0.5, std=0.5))
+            output = fn_tonumpy(fn_class(output))
+
+            writer_val.add_image('label', label, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+            writer_val.add_image('input', input, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+            writer_val.add_image('output', output, num_batch_train * (epoch - 1) + batch, dataformats='NHWC')
+
+    # loss를 Tensorboard에 저장
+    writer_val.add_scalar('loss', np.mean(loss_arr), epoch)
+
+    if epoch % 5 == 0:      # epoch이 n번 지날떄마다 저장
+        save(ckpt_dir=ckpt_dir, net=net, optim=optim, epoch=epoch)
+
+
+writer_train.close()
+writer_val.close()
 
