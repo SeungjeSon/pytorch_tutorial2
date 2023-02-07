2b71de1bdd978634b44aea47e5a9e31985a31660
SJ <seungje0927@gmail.com>
2023-02-07 10:06:00 +0900

    add Kaggle data series ch_1 small train

---
+++ TranferLearning/ch1_small_train_kaggle.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import transforms, datasets
+
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+
+
